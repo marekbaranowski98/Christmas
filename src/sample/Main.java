c07fea2ea9151427cd5fd32c1a77fb92852e6c37
Marek Baranowski <baranowski.marek.98@gmail.com>
2019-12-19 20:16:03 +0100

    Create empty window

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,20 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Merry Christmas");
+
+        FlowPane root = new FlowPane();
+
+        primaryStage.setScene(new Scene(root, 350, 500));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {

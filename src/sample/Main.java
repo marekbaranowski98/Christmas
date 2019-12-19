f2b1ac539f7523e5058e20daf28d8a38dbfb35cd
Marek Baranowski <baranowski.marek.98@gmail.com>
2019-12-19 21:40:09 +0100

    Set background black and alignment center

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,16 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
@@ -12,6 +19,8 @@ public class Main extends Application {
         primaryStage.setTitle("Merry Christmas");
 
         FlowPane root = new FlowPane();
+        root.setAlignment(Pos.CENTER);
+        root.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
 
         primaryStage.setScene(new Scene(root, 350, 500));
         primaryStage.show();

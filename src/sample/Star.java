34b3fe20b8ac1e61cf7e5b522eb9a04469d0c7d1
Marek Baranowski <baranowski.marek.98@gmail.com>
2019-12-20 19:15:34 +0100

    Change color star to gold

--- src/sample/Star.java
+++ src/sample/Star.java
@@ -7,7 +7,7 @@ public class Star {
   public double[] x = new double[] {-4, -1, 0, 1, 4, 2, 4, 0, -4, -2};
   public double[] y = new double[] {-1, -1, -3, -1, -1, 1, 4, 2, 4, 1};
 
-  public final Color color = Color.LIGHTGOLDENRODYELLOW;
+  public final Color color = Color.GOLD;
 
   public Star(double vectorX, double vectorY, double size) {
     n = x.length;

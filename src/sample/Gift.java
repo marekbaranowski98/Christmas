65aedd416983cef92d0972ab7350b66ac164468a
Marek Baranowski <baranowski.marek.98@gmail.com>
2019-12-20 21:37:19 +0100

    Create class gift

--- src/sample/Gift.java
+++ src/sample/Gift.java
@@ -1,4 +1,11 @@
 package sample;
 
+import javafx.scene.paint.Color;
+
 public class Gift {
+  private int width;
+  private int height;
+  private Color colorOfBox;
+  private int sizeOfRibbon;
+  private Color colorOfRibbon;
 }
